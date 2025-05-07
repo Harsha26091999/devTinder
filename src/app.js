303c973516ff2591d496821f975ad9732fda381f
HarshaMallimala <hmallimala@tradesun.com>
2025-05-07 11:21:57 +0530

    Details about routes and its extensions

--- src/app.js
+++ src/app.js
@@ -14,8 +14,21 @@ app.use("/hello",(req,res)=> {
     res.send('Response from hello');
 });
 
-app.use((req, res)=> {
+// If we add this as a first route then all the rutes will match with this and prints Hello
+app.use("/",(req, res)=> {
     res.send('Hello');
 });
 
 app.listen(7777);
+
+/*
+app.use will applicable for all the http methods get,post,put, patch and delete
+if you want to specify the http method then we need to use predefuined methods like
+app.get
+app.post etc
+
+Order of the route is very important, if we add app.use('/') at first then all the routes will match that
+route and executes that function it is like
+'/' = /*
+'/test/ = /test/*
+*/
