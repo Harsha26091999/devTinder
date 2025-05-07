4cd18e9e5a5e888833e3fb3aad00030ce247a494
HarshaMallimala <hmallimala@tradesun.com>
2025-05-07 17:07:45 +0530

    Wild card error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {auth} = require('./middleware/auth');
 
 const app = express();
 
@@ -7,15 +6,20 @@ const app = express();
  * Added middleware seperate file and used it in app.js
  */
 
-app.use("/user", auth);
 
 app.get("/user", (req, res, next) => {
-    res.send('List of Users');
+    throw Error('Erorr');
+    // res.send('List of Users');
 });
 
 app.post("/user", (req, res, next) => {
     res.send('Single user');
 });
+
+// This is a wild card error handling before this always use try catch block
+app.use('/', (err, req, res, next) => {
+    res.status(500).send('Something went wrong');
+})
 
 
 app.listen(7777);
