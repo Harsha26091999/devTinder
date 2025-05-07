916bedb93117bd9d49f9f3579583a7006ac73b4d
HarshaMallimala <hmallimala@tradesun.com>
2025-05-07 10:14:13 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/test",(req,res)=> {
+    res.send('Response from test');
+});
+
+app.use("/hello",(req,res)=> {
+    res.send('Response from hello');
+});
+
+app.use((req, res)=> {
+    res.send('Hello');
+});
+
+app.listen(7777);
