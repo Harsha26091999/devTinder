8b6fe9ecc464a43a654f486e3f24c09cb1aa4177
HarshaMallimala <hmallimala@tradesun.com>
2025-05-07 11:50:45 +0530

    Used Different types for routes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,51 @@ const express = require('express');
 
 const app = express();
 
-
-app.get('/test',(req, res)=> {
-    res.send('Get Request from test');
+/*
+Here we have used "tes?t" from this the system came to know that s is an optional character here
+so it will work as same for both /test and /tet
+*/
+app.get("/tes?t", (req, res)=> {
+    res.send('? Optional character');
 });
 
-app.post('/test',(req, res)=> {
-    res.send('Post Request from test');
+/*
+Here it is a wild card it will accept "/test/abc",  "/test/123",  "/test/1s3" but not  "/test" because
+it expects something after test
+*/
+app.get("/test/*", (req, res)=> {
+    res.send('* matches all the cases');
 });
 
-app.put('/test',(req, res)=> {
-    res.send('Put Request from test');
+/*
+Here when we use + it will accept n number of charaxcters which is beffore + like  "/test", "/tesst", "/tesssst"
+*/
+app.get('/tes+t', (req, res) => {
+    res.send('+ can add n number of character which is before +');
 });
 
-app.patch('/test',(req, res)=> {
-    res.send('Patch Request from test');
+/**
+ * Hereit is a dynamic route where we can send dynamic data in the url
+ */
+app.get('/test/:id', (req, res) => {
+    res.send('route paramenters');
 });
 
-app.delete('/test',(req, res)=> {
-    res.send('Delete Request from test');
-});
+/**
+ * Hereit is a dynamic multiple  route where we can send dynamic data in the url
+ */
+app.get('/test/:userId/book/:bookId', (req, res) => {
+    res.send(req.params);
+  });
+
+
+
+/**
+ * Next we can use regex too as a route for more dynamic endpoints
+ */
+app.get(/\/test\/(\d+)/, (req, res) => {
+    res.send('regex route route');
+  });
 
 app.listen(7777);
 
@@ -36,3 +61,12 @@ route and executes that function it is like
 '/' = /*
 '/test/ = /test/*
 */
+
+/**
+ * Pattern	       Meaning	                          Example Route	       Matches
+?	         Optional character	                      /colou?r	            /color, /colour
+*	         Matches any string (wildcard)	          /file/*	            /file/a/b/c
++	         One or more repetitions (regex only)	  /^\/ab+c$/	        /abc, /abbc
+:	         Named route parameter	                  /user/:id	            /user/123
+()	         Grouping (in regex)	                  `/^/(foo	            bar)$/`
+ */
